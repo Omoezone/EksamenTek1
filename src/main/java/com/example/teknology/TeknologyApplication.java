4965ef0573348f0b34a3ed00197a694d37c9a4ea
Omoezone <30931803+Omoezone@users.noreply.github.com>
2021-01-23 11:48:33 +0100

    Initial commit

---
+++ src/main/java/com/example/teknology/TeknologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teknology/TeknologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
